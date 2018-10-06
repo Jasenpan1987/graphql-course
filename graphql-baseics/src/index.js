63d3060c5acb752287d4dd63ed6170f6b50893a5
Jasenpan1987 <jasenpan@gmail.com>
2018-10-06 23:26:11 +1000

    basic graphql server

--- graphql-baseics/src/index.js
+++ graphql-baseics/src/index.js
@@ -3,5 +3,37 @@ import {
 } from "graphql-yoga"
 
 // Type definitions (schema)
-
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String
+    age: Int
+    location: String
+  }
+`
 // Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "My first query..."
+    },
+    name() {
+      return "Foo bar"
+    },
+    age() {
+      return 100
+    },
+    location() {
+      return "Somewhere"
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => {
+  console.log("Starting server...")
+})
