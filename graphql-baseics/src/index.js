09e9149d737b4ace3a107251b127ba34cc4c8331
Jasenpan1987 <jasenpan@gmail.com>
2018-10-07 17:31:23 +1100

    relation data

--- graphql-baseics/src/index.js
+++ graphql-baseics/src/index.js
@@ -27,19 +27,54 @@ const samplePosts = [{
     id: "1",
     title: "Hello World",
     body: "What a beautiful day!",
-    published: true
+    published: true,
+    author: "abc123",
   },
   {
     id: "2",
     title: "I'm learning graphql",
     body: "It's fun and powerful",
-    published: true
+    published: true,
+    author: "abc123",
   },
   {
     id: "3",
     title: "My Next mission",
     body: "Learn how to use graphql with node and react",
-    published: false
+    published: false,
+    author: "def456",
+  },
+]
+
+const sampleComments = [{
+    id: "1",
+    text: "This is awesome",
+    author: "def456",
+    post: "1"
+  },
+  {
+    id: "2",
+    text: "This is good",
+    author: "def456",
+    post: "2"
+  },
+  {
+    id: "3",
+    text: "This is ok",
+    author: "ghi789",
+    post: "3"
+  },
+  {
+    id: "4",
+    text: "This is bad",
+    author: "abc123",
+    post: "1"
+  },
+  {
+    id: "5",
+    text: "This is garbage",
+    author: "ghi789",
+    post: "2"
   },
 ]
 
@@ -50,10 +85,9 @@ function includeIn(str, sub) {
 // Type definitions (schema)
 const typeDefs = `
   type Query {
-    me: User!
     users(query: String): [User!]!
-    post: Post!
     posts(query: String): [Post!]!
+    comments(query: String): [Comment!]!
   }
 
   type User {
@@ -61,6 +95,8 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -68,27 +104,45 @@ const typeDefs = `
     title: String!
     body: String
     published: Boolean!
+    author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
   }
 `
 // Resolvers
 const resolvers = {
+  User: {
+    posts(parent, args) {
+      return samplePosts.filter(post => post.author === parent.id)
+    },
+    comments(parent, args) {
+      return sampleComments.filter(comment => comment.author === parent.id)
+    }
+  },
+  Post: {
+    author(parent, args) {
+      // parent refers to the Post it currently running
+      return sampleUsers.find(user => user.id === parent.author)
+    },
+    comments(parent, args) {
+      return sampleComments.filter(comment => comment.post === parent.id)
+    }
+  },
+  Comment: {
+    author(parent, args) {
+      return sampleUsers.find(user => user.id === parent.author)
+    },
+    post(parent, args) {
+      return samplePosts.find(post => post.id === parent.post)
+    }
+  },
   Query: {
-    me() {
-      return {
-        id: "abc123",
-        name: "Foo bar",
-        email: "foo@bar.com",
-        age: null
-      }
-    },
-    post() {
-      return {
-        id: "post_1",
-        title: "My awesome post",
-        body: undefined,
-        published: true
-      }
-    },
     users(parent, args) {
       if (!args.query) {
         return sampleUsers
@@ -107,6 +161,17 @@ const resolvers = {
         title,
         body
       }) => [title, body].some(txt => includeIn(txt, query)))
+    },
+    comments(parent, args) {
+      const {
+        query
+      } = args;
+      if (!query) {
+        return sampleComments
+      }
+      return sampleComments.filter(({
+        text
+      }) => includeIn(text, query))
     }
   }
 }
