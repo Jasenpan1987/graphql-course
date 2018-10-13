0b877038dbf87021a7c9f6ff1687e80bcddc720e
Jasenpan1987 <jasenpan@gmail.com>
2018-10-13 21:37:31 +1100

    mutation 2

--- graphql-baseics/src/index.js
+++ graphql-baseics/src/index.js
@@ -1,10 +1,9 @@
-import {
-  GraphQLServer
-} from "graphql-yoga"
-import uuidv4 from "uuid/v4"
+import { GraphQLServer } from "graphql-yoga";
+import uuidv4 from "uuid/v4";
 
 // sample data
-const sampleUsers = [{
+const sampleUsers = [
+  {
     id: "abc123",
     name: "Foo bar",
     email: "foo@bar.com",
@@ -22,32 +21,34 @@ const sampleUsers = [{
     email: "john@doe.com",
     age: 80
   }
-]
+];
 
-const samplePosts = [{
+const samplePosts = [
+  {
     id: "1",
     title: "Hello World",
     body: "What a beautiful day!",
     published: true,
-    author: "abc123",
+    author: "abc123"
   },
   {
     id: "2",
     title: "I'm learning graphql",
     body: "It's fun and powerful",
     published: true,
-    author: "abc123",
+    author: "abc123"
   },
   {
     id: "3",
     title: "My Next mission",
     body: "Learn how to use graphql with node and react",
     published: false,
-    author: "def456",
-  },
-]
+    author: "def456"
+  }
+];
 
-const sampleComments = [{
+const sampleComments = [
+  {
     id: "1",
     text: "This is awesome",
     author: "def456",
@@ -76,8 +77,8 @@ const sampleComments = [{
     text: "This is garbage",
     author: "ghi789",
     post: "2"
-  },
-]
+  }
+];
 
 function includeIn(str, sub) {
   return str.toLowerCase().includes(sub.toLowerCase());
@@ -93,6 +94,8 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
 
   type User {
@@ -119,75 +122,64 @@ const typeDefs = `
     author: User!
     post: Post!
   }
-`
+`;
 // Resolvers
 const resolvers = {
   User: {
     posts(parent, args) {
-      return samplePosts.filter(post => post.author === parent.id)
+      return samplePosts.filter(post => post.author === parent.id);
     },
     comments(parent, args) {
-      return sampleComments.filter(comment => comment.author === parent.id)
+      return sampleComments.filter(comment => comment.author === parent.id);
     }
   },
   Post: {
     author(parent, args) {
       // parent refers to the Post it currently running
-      return sampleUsers.find(user => user.id === parent.author)
+      return sampleUsers.find(user => user.id === parent.author);
     },
     comments(parent, args) {
-      return sampleComments.filter(comment => comment.post === parent.id)
+      return sampleComments.filter(comment => comment.post === parent.id);
     }
   },
   Comment: {
     author(parent, args) {
-      return sampleUsers.find(user => user.id === parent.author)
+      return sampleUsers.find(user => user.id === parent.author);
     },
     post(parent, args) {
-      return samplePosts.find(post => post.id === parent.post)
+      return samplePosts.find(post => post.id === parent.post);
     }
   },
 
   Query: {
     users(parent, args) {
       if (!args.query) {
-        return sampleUsers
+        return sampleUsers;
       }
 
-      return sampleUsers.filter(user => includeIn(user.name, args.query))
+      return sampleUsers.filter(user => includeIn(user.name, args.query));
     },
     posts(parent, args) {
-      const {
-        query
-      } = args;
+      const { query } = args;
       if (!query) {
         return samplePosts;
       }
-      return samplePosts.filter(({
-        title,
-        body
-      }) => [title, body].some(txt => includeIn(txt, query)))
+      return samplePosts.filter(({ title, body }) =>
+        [title, body].some(txt => includeIn(txt, query))
+      );
     },
     comments(parent, args) {
-      const {
-        query
-      } = args;
+      const { query } = args;
       if (!query) {
-        return sampleComments
+        return sampleComments;
       }
-      return sampleComments.filter(({
-        text
-      }) => includeIn(text, query))
+      return sampleComments.filter(({ text }) => includeIn(text, query));
     }
   },
 
   Mutation: {
     createUser(parent, args, ctx, info) {
-      const {
-        name,
-        email,
-        age
-      } = args;
+      const { name, email, age } = args;
 
       const emailTaken = sampleUsers.some(user => user.email === email);
 
@@ -200,19 +192,68 @@ const resolvers = {
         name,
         email,
         age
-      }
+      };
       sampleUsers.unshift(newUser);
 
       return newUser;
+    },
+
+    createPost(parent, args, ctx, info) {
+      const { author, title, body, published } = args;
+
+      const userExist = sampleUsers.some(user => user.id === author);
+      if (!userExist) {
+        throw new Error("User does not exist");
+      }
+
+      const newPost = {
+        id: uuidv4(),
+        title,
+        body,
+        published,
+        author
+      };
+
+      samplePosts.unshift(newPost);
+
+      return newPost;
+    },
+
+    createComment(parent, args, ctx, info) {
+      const { text, author, post } = args;
+      const authorFound = sampleUsers.some(user => user.id === author);
+      const postFound = samplePosts.find(p => p.id === post);
+      if (!postFound) {
+        throw new Error("Post does not exist");
+      }
+
+      if (!postFound.published) {
+        throw new Error("Post is not published");
+      }
+
+      if (!authorFound) {
+        throw new Error("Author not found");
+      }
+
+      const newComment = {
+        id: uuidv4(),
+        text,
+        author,
+        post
+      };
+
+      sampleComments.push(newComment);
+
+      return newComment;
     }
   }
-}
+};
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers
-})
+});
 
 server.start(() => {
-  console.log("Starting server...")
-})
+  console.log("Starting server...");
+});
